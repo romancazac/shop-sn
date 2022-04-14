2123f8ae6f0201aa93db7e5938b3ad6685c52342
Roman Cazac <romancazac23@icloud.com>
2022-04-14 17:27:38 +0300

    Updates

---
+++ precache-manifest.724021a0ac886b8354b608f45a504c1d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7a8abdcd7ee498ed077",
+    "url": "/shop-sn/static/css/main.9059b91e.chunk.css"
+  },
+  {
+    "revision": "f7a8abdcd7ee498ed077",
+    "url": "/shop-sn/static/js/main.f7a8abdc.chunk.js"
+  },
+  {
+    "revision": "62143d3118958bf34fbb",
+    "url": "/shop-sn/static/css/1.b2491226.chunk.css"
+  },
+  {
+    "revision": "62143d3118958bf34fbb",
+    "url": "/shop-sn/static/js/1.62143d31.chunk.js"
+  },
+  {
+    "revision": "f7df02b9f80f925fa66f",
+    "url": "/shop-sn/static/js/runtime~main.f7df02b9.js"
+  },
+  {
+    "revision": "86b55a7386a308e2d2257f5ca13aa3ba",
+    "url": "/shop-sn/static/media/cart.86b55a73.svg"
+  },
+  {
+    "revision": "59f251e82d23b648a44234a5ea9fe908",
+    "url": "/shop-sn/static/media/wish.59f251e8.svg"
+  },
+  {
+    "revision": "b38551f1c72c75a6d8e05318a64b44a3",
+    "url": "/shop-sn/static/media/user.b38551f1.svg"
+  },
+  {
+    "revision": "ccc270a0337ab2172db59e119090cb1f",
+    "url": "/shop-sn/static/media/plus.ccc270a0.svg"
+  },
+  {
+    "revision": "0c8ee45c23fe0ace45057b6aeccaf5f5",
+    "url": "/shop-sn/static/media/ch.0c8ee45c.svg"
+  },
+  {
+    "revision": "740dde134e3b5eb6aea5fa98fc68c1ae",
+    "url": "/shop-sn/static/media/slide-img.740dde13.png"
+  },
+  {
+    "revision": "3ec39de38258f8b5babc72946c5d2f2f",
+    "url": "/shop-sn/static/media/ar-r.3ec39de3.svg"
+  },
+  {
+    "revision": "48c816d68cfaf3b10224a4c97c56eb28",
+    "url": "/shop-sn/static/media/close.48c816d6.svg"
+  },
+  {
+    "revision": "87cacf007e755dd9bdc01fca127e2bf0",
+    "url": "/shop-sn/static/media/l.87cacf00.svg"
+  },
+  {
+    "revision": "be41b755ade3e035be8e3eca2bdbb7b2",
+    "url": "/shop-sn/static/media/search.be41b755.svg"
+  },
+  {
+    "revision": "fdc8985ed33d186d81677f527c0bbba9",
+    "url": "/shop-sn/index.html"
+  }
+];
